9582458a6004afebac9e83c081814af2119b97ba
Mark <markandrewhamm@gmail.com>
2020-09-01 18:43:30 -0500

    fixed user show to look like session show

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -6,17 +6,18 @@
           <div class="col-xl-9 mx-auto">
             <div class="cta-inner text-center rounded">
               <h2 class="section-heading mb-4">
-                <span class="section-heading-lower">{{ this.user.username }}</span>
-              </h2>
-              <div v-for="session in this.user.sessions">
-                <p class="mb-0">State: {{ session.state }}</p>
-                <p class="mb-0">County: {{ session.county }}</p>
-                <span class="section-heading-upper">{{moment(session.created_at).format('MMMM Do YYYY, h:mm a')}}</span>
-                <div class="intro-button mx-auto">
-                  <a class="btn btn-primary btn-xl" v-bind:href="'/sessions/' + session.id">View Birds</a>
-                </div>
+                <span class="section-heading-lower">{{ this.user.username }}'s Sessions</span>
                 <hr>
-              </div>
+                <div v-for="session in this.user.sessions">
+                  <span class="section-heading-upper">{{moment(session.created_at).format('MMMM Do YYYY, h:mm a')}}</span>
+                  <span class="section-heading-lower">State: {{ session.state }}</span>
+                  <span class="section-heading-lower">County: {{ session.county }}</span>
+                  <div class="intro-button mx-auto">
+                    <a class="btn btn-primary btn-xl" v-bind:href="'/sessions/' + session.id">View Birds</a>
+                  </div>
+                  <hr>
+                </div>
+              </h2>
             </div>
           </div>
         </div>
