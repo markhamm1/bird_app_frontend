814c9c981bbca65dc917acae6de0a2849a48e47a
Mark <markandrewhamm@gmail.com>
2020-09-13 20:30:31 -0500

    changed router link after login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -59,7 +59,7 @@ export default {
         .then((response) => {
           axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
-          this.$router.push("/new");
+          this.$router.push("/sessions");
         })
         .catch((error) => {
           this.errors = ["Invalid email or password."];
